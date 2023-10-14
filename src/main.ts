407aa4add20d5c2ff1958b0ec65cde95a2c80f1f
TheBlackbird14 <90150013+TheBlackbird14@users.noreply.github.com>
2023-10-14 22:30:36 +0200

    added cors options

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Set up CORS options
+  const corsOptions: CorsOptions = {
+    origin: '*', // Allow requests from any origin. You can specify specific origins if needed.
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Specify the allowed HTTP methods.
+    credentials: true, // Allow cookies and other credentials to be sent.
+    optionsSuccessStatus: 204, // Set the preflight response status (204 means "No Content").
+  };
+
+  app.enableCors(corsOptions);
+
   await app.listen(3000);
 }
 bootstrap();
