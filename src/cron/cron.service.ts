41230ae9f4d81adfb24181ef22b03e2ccb32f578
TheBlackbird14 <90150013+TheBlackbird14@users.noreply.github.com>
2024-01-07 23:19:24 +0100

    fix importing of food schedule
    - import one week ahead
    - add padding to weeknumber to avoid 404
    - add failsafe for week with no food

--- src/cron/cron.service.ts
+++ src/cron/cron.service.ts
@@ -34,9 +34,10 @@ export class CronService {
     const days = Math.floor(
       (now.getTime() - startDate.getTime()) / (24 * 60 * 60 * 1000),
     );
-    const weekNumber = Math.ceil(days / 7);
+    const weekNumber = Math.ceil(days / 7) + 1;
 
-    const weekString = now.getFullYear() + 'W' + weekNumber;
+    const weekString =
+      now.getFullYear() + 'W' + weekNumber.toString().padStart(2, '0');
 
     await this.fetchService.importFood(weekString);
   }
